01849836e3308eb26d079b66d6eef735e5e5f82a
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-07-14 23:41:07 +0900

    init  初期表示で地球儀が表示されるように変更

--- cesium-ts-app/src/initCesiumJS.ts
+++ cesium-ts-app/src/initCesiumJS.ts
@@ -30,15 +30,16 @@ export const initCesium = async () => {
     vrButton: false,
   });
 
-  // 初期位置を東京に設定
-  viewer.camera.setView({
-    destination: Cartesian3.fromDegrees(139.6917, 35.6895, 10000),
-    orientation: {
-      heading: Math.toRadians(0),
-      pitch: Math.toRadians(-90),
-      roll: 0,
-    },
-  });
+  // 初期表示時で地球儀表示
+  // // 初期位置を東京に設定
+  // viewer.camera.setView({
+  //   destination: Cartesian3.fromDegrees(139.6917, 35.6895, 10000),
+  //   // orientation: {
+  //   //   heading: Math.toRadians(0),
+  //   //   pitch: Math.toRadians(-90),
+  //   //   roll: 0,
+  //   // },
+  // });
 };
 
 // 起動
